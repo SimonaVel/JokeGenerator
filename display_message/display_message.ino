f9da134a6636c1c790c65499f7cea12be48d24c4
pren0Sima <108150997+pren0Sima@users.noreply.github.com>
2024-04-17 20:21:38 +0200

    Update display_message.ino
    Abstracted the setup for the display in a separate function.

--- display_message/display_message.ino
+++ display_message/display_message.ino
@@ -5,26 +5,30 @@
 
 #include <LiquidCrystal_I2C.h>
 
-// set the LCD number of columns and rows
-int lcdColumns = 16;
-int lcdRows = 2;
-
-// set LCD address, number of columns and rows
-// if you don't know your display address, run an I2C scanner sketch
-LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);  
-
-void setup(){
+void setUpDisplayMessage() {
+  // set the LCD number of columns and rows
+  int lcdColumns = 16;
+  int lcdRows = 2;
+  // set LCD address, number of columns and rows
+  // if you don't know your display address, run an I2C scanner sketch
+  LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);  
   // initialize LCD
   lcd.init();
   // turn on LCD backlight                      
   lcd.backlight();
-}
-
-void loop(){
   // set cursor to first column, first row
   lcd.setCursor(0, 0);
   // print message
   lcd.print("Another joke?");
   lcd.setCursor(0, 1);
   lcd.print("Press the button");
+}
+
+
+void setup(){
+  setUpDisplayMessage();
+}
+
+void loop(){
+  
 }
